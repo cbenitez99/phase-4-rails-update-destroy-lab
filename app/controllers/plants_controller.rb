4737b3cd0f720f127778458ad285db2f548cd7d4
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-11-20 01:57:00 -0600

    commit tree 9ca97e536ed6de38000e1e9b2787c102ad2d555c

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,31 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  rescue
+    render_not_found
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
+
+  def render_not_found
+    render json: {error: "Plant not found"}, status: :not_found
   end
 end
